06196a533e424c7c6b782f37ee2e6b036cbd9967
Brad Edwards <j.bradley.edwards@gmail.com>
2025-02-23 15:35:47 -0800

    Add subset generation

--- src/cs/combinatorial.rs
+++ src/cs/combinatorial.rs
@@ -3,9 +3,11 @@ pub mod dancing_links;
 pub mod johnson_trotter;
 pub mod gray_code;
 pub mod zassenhaus;
+pub mod subset_gen;
 
 pub use backtracking::{combinations, permutations};
 pub use dancing_links::DancingLinks;
 pub use johnson_trotter::johnson_trotter;
 pub use gray_code::gray_code;
 pub use zassenhaus::{Permutation, PermutationGroup, schur_zassenhaus};
+pub use subset_gen::{power_set, power_set_iter, PowerSet};
